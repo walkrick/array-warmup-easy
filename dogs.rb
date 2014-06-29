76d7279b0cb1f3a97fe57851256fa196e2cecd26
Rick Walker <walkrick@yahoo.com>
2014-06-29 10:42:01 -0600

    finished

--- dogs.rb
+++ dogs.rb
@@ -3,34 +3,67 @@ require_relative "testing_library"
 dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
 
 def how_many_dogs(dogs)
+  dogs.length
 
 end
 
 def name_lengths(dogs)
+  dogs.collect do |dog|
+    dog.length
 
+  end
 end
 
+
 def reverse_dog_names(dogs)
+
+  dogs.map { |dog| dog.reverse }
 
 end
 
 def first_three_dogs_with_each(dogs)
-
+  first_three = []
+  dogs.each_with_index do |dog, index|
+    if index == 3
+      return first_three
+    end
+    first_three.push(dog)
+  end
 end
 
 def first_three_dogs_without_each(dogs)
+
+  dogs[0..2]
 
 end
 
 def reverse_case_dog_names(dogs)
 
+  dogs.map { |dog| dog.swapcase }
+
 end
 
 def sum_of_all_dog_name_lengths(dogs)
 
+  dog_sum = 0
+  dogs.each do |dog|
+    dog_sum = dog_sum + dog.length
+  end
+  dog_sum
 end
 
 def dogs_with_long_names(dogs)
+
+  long = []
+
+  dogs.each do |dog|
+    if dog.length > 4
+      long << true
+    else
+      long << false
+    end
+  end
+long
 
 end
 
